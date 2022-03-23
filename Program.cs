914dbf2b77bfcc0e53c6ddcc39156919ffe2f6de
murgendrag <murgendra67@gmail.com>
2022-03-23 22:11:04 +0530

    call in main

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericstestMaximum_Day13
+{
+    public class Program
+    {
+       static void Main(string[] args)
+        {
+            int d;
+            float g;
+            TestNumbers<int> obj = new TestNumbers<int>(45,56,67);
+            d = obj.findmaximumnumber();
+            Console.WriteLine("maximum value" + d);
+
+           
+
+        }
+    }
+}
